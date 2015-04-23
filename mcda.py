a384749a3942f304864e10a1936b3b1dad9bd2d2
Tudor Ambarus <tudor.ambarus@gmail.com>
2015-04-23 08:53:53 -0400

    add mcda algorithm
    Signed-off-by: Tudor Ambarus <tudor.ambarus@gmail.com>

--- mcda.py
+++ mcda.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from __future__ import division
+from collections import defaultdict
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -134,19 +135,106 @@ def inter_controller_latency(g_path_len, ci):
 
     return max(icl_path_len)
 
-if __name__ == '__main__':
-    elist = [(1,5,4),(1,6,2),(2,3,5),(2,4,2),(2,5,3),(2,6,6),(3,2,5),(3,5,5),(3,6,2),(4,2,2),(4,5,1),(4,6,4),(5,1,4),(5,2,3),(5,3,5),(5,4,1),(5,6,3),(6,1,2),(6,2,6),(6,3,2),(6,4,4),(6,5,3)]
-    cplacement = {1: {1: 0, 2: 0, 3: 0, 4: 0, 5: 1, 6: 1}, 2: {1: 0, 2: 0, 3: 1, 4: 0, 5: 1, 6: 0}, 3: {1: 0, 2: 0, 3: 1, 4: 0, 5: 0, 6: 1}, 4: {1: 0, 2: 0, 3: 0, 4: 1, 5: 0, 6: 1}}
+def normalize_dparam(dparam, weight, r, a):
+    """Normalize decision parameter
 
-    args=parse_args()
+    Parameters
+    ----------
+    dparam : decision parameter
+    weght : weight of decision parameter
+    r : reservation level
+    a : aspiration level
+
+    Returns
+    -------
+    Normalized decision variable.
+    """
+    return weight * (r - dparam) / (r - a)
+
+if __name__ == '__main__':
+    elist = [(1,5,4),(1,6,2),(2,3,5),(2,4,2),(2,5,3),(2,6,6),(3,2,5),(3,5,5),
+             (3,6,2),(4,2,2),(4,5,1),(4,6,4),(5,1,4),(5,2,3),(5,3,5),(5,4,1),
+             (5,6,3),(6,1,2),(6,2,6),(6,3,2),(6,4,4),(6,5,3)]
+    cplacement = {1: {1: 0, 2: 0, 3: 0, 4: 0, 5: 1, 6: 1},
+                  2: {1: 0, 2: 0, 3: 1, 4: 0, 5: 1, 6: 0},
+                  3: {1: 0, 2: 0, 3: 1, 4: 0, 5: 0, 6: 1},
+                  4: {1: 0, 2: 0, 3: 0, 4: 1, 5: 0, 6: 1}}
+
+    # parse user arguments
+    args, nargs = parse_args()
 
     G = nx.Graph()
     G.add_nodes_from([1,6])
     G.add_weighted_edges_from(elist)
 
-    average_latency(G, cplacement)
-    worst_latency(G, cplacement)
-    inter_controller_latency(G, cplacement)
+    '''
+    Compute shortest path lengths between all nodes in a weighted graph.
+    g_path_len will be a dictionary, keyed by source and target, of shortest
+    path lengths
+    '''
+    g_path_len=nx.all_pairs_dijkstra_path_length(G)
+
+    '''
+    Construct decision parameters dictionary of dictionaries.
+    dparam will be keyed by decision parameter and Ci placement.
+    '''
+    dparams = defaultdict(dict)
+    for ci, ci_value in cplacement.iteritems():
+        if args.a:
+            dparams[1][ci] = average_latency(g_path_len, ci_value)
+        if args.w:
+            dparams[2][ci] = worst_latency(g_path_len, ci_value)
+        if args.i:
+            dparams[3][ci] = inter_controller_latency(g_path_len, ci_value)
+
+    # use static levels in order to check the correctness of algorithms
+    if False:'''
+    for k, v in dparams.iteritems():
+        # set reservation level
+        r.append(max(v.itervalues()))
+        # set aspiration level
+        a.append(min(v.itervalues()))
+    '''
+    else:
+        # static reservation level
+        r = [3, 6, 6]
+        # static aspiration level
+        a = [0, 0, 0]
+
+    # construct list of arguments
+    largs = []
+    if args.a:
+        largs.append(args.a)
+    if args.w:
+        largs.append(args.w)
+    if args.i:
+        largs.append(args.i)
+
+    for k, v in dparams.iteritems():
+        # lists are indexed starting with int(0) value
+        t = k-1
+        for i in range (1,ci+1):
+            dparams[k][i] = normalize_dparam(dparams[k][i], largs[t], r[t], a[t])
+
+    '''
+    Transposed decision parameters dictionaty of dictionaries.
+    t_dparams will be keyed by Ci placement and decision parameter.
+    '''
+    t_dparams = defaultdict(dict)
+    for i, v in dparams.iteritems():
+        for j in range(1,ci+1):
+            t_dparams[j][i] = dparams[i][j]
+
+    '''
+    Compute for each candidate solution Ci, the minimum among
+    all its normalized decision parameters / variables.
+    '''
+    min_list = []
+    for i, v in t_dparams.iteritems():
+        min_list.append(min(v.itervalues()))
+
+    # make selection among solutions
+    print (min_list.index(max(min_list)) + 1)
 
     # positions for all nodes
     pos=nx.spring_layout(G)
@@ -157,5 +245,6 @@ if __name__ == '__main__':
     # nx.draw_networkx_edge_labels(G,pos)
     edge_labels=dict([((u,v,),d['weight']) for u,v,d in G.edges(data=True)])
     nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
-    # display
-    #plt.show()
+
+    # display graph
+    plt.show()
