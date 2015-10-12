9a045013c737426d379583ef26d1b9346d4ad2f3
Tudor Ambarus <tudor.ambarus@gmail.com>
2015-10-12 10:44:08 -0400

    refactoring: create draw module
    Signed-off-by: Tudor Ambarus <tudor.ambarus@gmail.com>

---
+++ draw.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def print_ci_info(min_list, cplacement, args):
+    result = min_list.index(max(min_list))
+
+    #in static example, first ci placement is 1 and not 0
+    if not args.dynamic:
+        result += 1
+
+    print "Optimum Ci placement is Ci =", result
+
+    for key, value in cplacement[result].iteritems():
+        if value:
+            print "Controller is placed in node", key
+
+
+def draw_graph(G, labels=None, graph_layout='spring',
+               node_size=1200, node_color='blue', node_alpha=0.3,
+               node_text_size=12,
+               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               edge_text_pos=0.3,
+               text_font='sans-serif'):
+
+    """
+    these are different layouts for the network you may try
+    shell seems to work best
+    """
+    if graph_layout == 'spring':
+        graph_pos=nx.spring_layout(G)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(G)
+    else:
+        graph_pos=nx.shell_layout(G)
+
+    # draw graph
+    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size,
+                           alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
+                           alpha=edge_alpha,edge_color=edge_color)
+    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
+                            font_family=text_font)
+
+    """
+    edge_labels=dict([((u,v,),d['weight']) for u,v,d in G.edges(data=True)])
+    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels,
+                                 label_pos=edge_text_pos)
+    """
+    plt.xlim(-0.05,1.05)
+    plt.ylim(-0.05,1.05)
+    plt.axis('off')
+    # show graph
+    plt.show()
+
